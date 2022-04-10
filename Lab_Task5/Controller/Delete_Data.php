a2b37e0129c7bc668365a1efa64b40da15342cda
Rishad-prog <ihrishad10@gmail.com>
2022-04-10 23:53:55 +0600

    lab task

---
+++ Lab_Task5/Controller/Delete_Data.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once '../Model/model.php';
+
+if (deleteData($_GET['id'])) {
+    header('Location:../View/Display_Profit.php');
+}
---
+++ Lab_Task5/Controller/Show_Data.php
@@ -0,0 +1,10 @@
+<?php
+require_once '../Model/model.php';
+function show_profit()
+{
+    return showallData();
+}
+function edit_property($id)
+{
+    return editData($id);
+}
